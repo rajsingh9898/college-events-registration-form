42a19cb5f13361bb361b62135c94b817e0384eff
Raj Singh <rajsingh98447859@gmail.com>
2026-01-24 21:30:22 +0530

    Restructured project folders and updated README with screenshots

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUserDashboard, registerForSoloEvent } = require('../controllers/userController');
-const { authMiddleware } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/dashboard', authMiddleware, getUserDashboard);
-router.post('/register-solo', authMiddleware, registerForSoloEvent);
-
-module.exports = router;
